db79880b6d11cab5225f312c4d84b39eb641e37d
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-10-21 19:32:04 +0200

    :sparkles: feat(Users Service, Gateway): Config users service for start with docker
    Configuration of the docker compose for start the gateway and the users service

--- Platform/Server/Services/apps/gateway/src/app.service.ts
+++ Platform/Server/Services/apps/gateway/src/app.service.ts
@@ -3,6 +3,6 @@ import {Injectable} from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello World!";
+        return "Gateway API started on 4000!";
     }
 }
--- Platform/Server/Services/apps/users/src/main.ts
+++ Platform/Server/Services/apps/users/src/main.ts
@@ -3,7 +3,7 @@ import {UsersModule} from "./users.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(UsersModule);
-    await app.listen(process.env.port ?? 3000);
+    await app.listen(process.env.PORT_USERS || 4001);
 }
 
 bootstrap();
